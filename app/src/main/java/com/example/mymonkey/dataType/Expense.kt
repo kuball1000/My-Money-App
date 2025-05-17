dbb18b86ebbe70d6a2af8fe0edc87e7636d93d06
kuball1000 <jk.buler@gmail.com>
2025-05-18 01:07:08 +0200

    dodanie dataTypu dla wydatku

---
+++ app/src/main/java/com/example/mymonkey/dataType/Expense.kt
@@ -0,0 +1,9 @@
+package com.example.mymonkey.dataType
+
+data class Expense(
+    val id: Int,
+    val description: String,
+    val amount: Double,
+    val location: String,
+    val coordinates: String
+)
